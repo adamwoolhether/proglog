2f6510547d9e4dea6b6ba25438e4887785c320ba
adamwoolhether <adamwoolhether@gmail.com>
2021-08-18 09:40:43 +0800

    implemented FSM logstore and snapshot

--- internal/log/distributed.go
+++ internal/log/distributed.go
@@ -2,7 +2,7 @@ package log
 
 import (
 	"bytes"
-	"crypto/tls"
+	"io"
 	"os"
 	"path/filepath"
 	"time"
@@ -134,18 +134,206 @@ func (l *DistributedLog) Append(record *api.Record) (uint64, error) {
 	return res.(*api.ProduceResponse).Offset, nil
 }
 
+// apply wraps Raft's API to apply requests and return their response.
+// It marshals the request type and request into bytes used by Raft
+// to record data is replicates.
 func (l *DistributedLog) apply(reqType RequestType, req proto.Message) (interface{}, error) {
+	var buf bytes.Buffer
+	_, err := buf.Write([]byte{byte(reqType)})
+	if err != nil {
+		return nil, err
+	}
 
+	b, err := proto.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = buf.Write(b)
+	if err != nil {
+		return nil, err
+	}
+
+	// Apply the command to FSM, replicating the record and appending to the leader's log
+	timeout :=10 *time.Second
+	future := l.raft.Apply(buf.Bytes(), timeout)
+	if future.Error() != nil {
+		return nil, future.Error()
+	}
+
+	res := future.Response()
+	if err, ok := res.(error); ok {
+		return nil, err
+	}
+	return res, nil
 }
+
+// Read reads the recor's offset from the server's log.
+func (l *DistributedLog) Read(offset uint64) (*api.Record, error) {
+	return l.log.Read(offset)
+}
+
+// Define Finite-State Machine, which needs to access the data it manages.
+// It must implement three methods: Apply, Snapshot, and Restore.
+var _ raft.FSM = (*fsm)(nil)
 
 type fsm struct {
 	log *Log
 }
 
-func newLogStore(dir string, c Config) (*logStore, error) {
+type RequestType uint8
 
+const (
+	AppendRequestType RequestType = 0
+)
+
+// Apply reqds requests, identifies their type, and applies them.
+// Currently, there is only one, but other methods can me implemented
+// and called upon.
+func (l *fsm) Apply(record *raft.Log) interface{} {
+	buf := record.Data
+	reqType := RequestType(buf[0])
+	switch reqType {
+	case AppendRequestType:
+		return l.applyAppend(buf[1:])
+	}
+	return nil
+}
+
+// applyAppend unmarshals the request and appeds the record to the
+// local log, returning the Raft respones, back to where raft.Apply
+// was called in DistributedLog.Append
+func (l *fsm) applyAppend(b []byte) interface{} {
+	var req api.ProduceRequest
+	err := proto.Unmarshal(b, &req)
+	if err != nil {
+		return nil
+	}
+
+	offset, err := l.log.Append(req.Record)
+	if err != nil {
+		return nil
+	}
+
+	return &api.ProduceResponse{Offset: offset}
+}
+
+// Snapshot returns and FSM snapshot representing a point-in-time
+// snapshot of the FSM's state(in this case, the FSM log).
+func (f *fsm) Snapshot() (raft.FSMSnapshot, error) {
+	r := f.log.Reader()
+	return &snapshot{reader: r}, nil
+}
+
+var _ raft.FSMSnapshot = (*snapshot)(nil)
+
+type snapshot struct {
+	reader io.Reader
+}
+
+// Persist writes the snapshot's state to a sink, which could be
+// in-memory, a file, etc. to store the bytes.
+func (s *snapshot) Persist(sink raft.SnapshotSink) error {
+	if _, err := io.Copy(sink, s.reader); err != nil {
+		_ = sink.Cancel()
+		return err
+	}
+	return sink.Close()
+}
+
+func (s *snapshot) Release() {}
+
+// Restore is called by Raft to restore an FSM from snapshot.
+// It resets the log, configures its initial offset to the
+// first record's offset read from the snapshot, and then
+// appends the snapshot's record to the new log.
+func (f *fsm) Restore(r io.ReadCloser) error {
+	b := make([]byte, lenWidth)
+	var buf bytes.Buffer
+
+	for i := 0; ; i++ {
+		_, err := io.ReadFull(r, b)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+		size := int64(enc.Uint16(b))
+		if _, err = io.CopyN(&buf, r, size); err != nil {
+			return err
+		}
+		record := &api.Record{}
+		if err = proto.Unmarshal(buf.Bytes(), record); err != nil {
+			return err
+		}
+		if i == 0 {
+			f.log.Config.Segment.InitialOffset = record.Offset
+			if err := f.log.Reset(); err != nil {
+				return err
+			}
+		}
+		if _, err := f.log.Append(record); err != nil {
+			return err
+		}
+		buf.Reset()
+	}
+	return nil
 }
 
 type logStore struct {
 	*Log
+}
+
+func newLogStore(dir string, c Config) (*logStore, error) {
+	log, err := NewLog(dir, c)
+	if err != nil {
+		return nil, err
+	}
+	return &logStore{log}, nil
+}
+
+// The following APIs are used by Raft to get records and
+// information about the log. To us, our offset is what
+// Raft calls 'indexes'.
+
+func (l *logStore) FirstIndex() (uint64, error) {
+	return l.LowestOffset()
+}
+
+func (l *logStore) LastIndex() (uint64, error) {
+	off, err := l.HighestOffset()
+	return off, err
+}
+
+func (l *logStore) GetLog(index uint64, out *raft.Log) error {
+	in, err := l.Read(index)
+	if err != nil {
+		return err
+	}
+	out.Data = in.Value
+	out.Index = in.Offset
+	out.Type = raft.LogType(in.Type)
+	out.Term = in.Term
+	return nil
+}
+
+func (l *logStore) StoreLog(record *raft.Log) error {
+	return l.StoreLogs([]*raft.Log{record})
+}
+
+func (l *logStore) StoreLogs(records []*raft.Log) error {
+	for _, record := range records {
+		if _, err := l.Append(&api.Record{
+			Value: record.Data,
+			Term: record.Term,
+			Type: uint32(record.Type),
+		}); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *logStore) DeleteRange(min, max uint64) error {
+	return l.Truncate(max)
 }
